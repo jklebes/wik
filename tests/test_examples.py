9eb3ab10670c66e4979f683ebf5dbe415aa0e850
jsk <jsklebes@googlemail.com>
2024-05-26 09:17:14 +0100

    My tests on hatch package branch
    including Lahore case

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 import unittest
+from unittest.mock import patch
+from io import StringIO
 import pytest
 from wik import info
 
 class FetchTest(unittest.TestCase):
-    def test_getSummary(self):
-        result = info.getSummary("Linux")
-        self.assertEqual(
-            result,
-            """-------------------------------------------------------Linux--------------------------------------------------------
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_getSummary(self, mock_stdout):
+        info.getSummary("Linux")
+        result=mock_stdout.getvalue()
+        self.assertTrue("Linux" in result)
 
-Linux (/ˈlinʊks/ (listen) LEEN-uuks or /ˈlɪnʊks/ LIN-uuks) is a family of open-source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991, by Linus Torvalds. Linux is typically packaged in a Linux distribution.
-
-
-Distributions include the Linux kernel and supporting system software and libraries, many of which are provided by the GNU Project. Many Linux distributions use the word "Linux" in their name, but the Free Software Foundation uses the name "GNU/Linux" to emphasize the importance of GNU software, causing some controversy.""",
-       )
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_Lahore(self, mock_stdout):
+        info.getSummary("Lahore")
+        result=mock_stdout.getvalue()
+        self.assertTrue(result is not None)
+        self.assertTrue("Lahore" in result)
+        self.assertTrue("Pakistan" in result)
 
 
